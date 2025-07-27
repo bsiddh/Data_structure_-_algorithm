6c75261b8df44e8e05c0d722c0b4e12ece357624
bsiddh <bsiddh667@gmail.com>
2025-07-27 23:05:32 +0530

    list has been updated

--- src/com/linkedlist/java/LinkedList.java
+++ src/com/linkedlist/java/LinkedList.java
@@ -6,22 +6,41 @@ public class LinkedList {
     private Node tail;
     private int length;
 
-    public LinkedList(int value){
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
-        length =1;
+        length = 1;
     }
 
+
+    class Node {
+        int value;
+        Node next;//it is pointer to the node.
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+        public void printList() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.value);
+                temp = temp.next;
+            }
+        }
+
+        public void getHead() {
+            System.out.println("Head: " + head.value);
+        }
+
+        public void getTail() {
+            System.out.println("Tail: " + tail.value);
+        }
+
+        public void getLength() {
+            System.out.println("Length: " + length);
+        }
+
+
 }
-  class   Node{
-       int value;
-       Node next;//it is pointer to the node.
-
-  Node(int value)
-  {
-      this.value=value;
-  }
-  }
-
-
